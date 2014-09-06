5c1c89e33ebddc4d3c8c18c2d9f7ece96d02b473
Raquel Peces <raquelpm23@gmail.com>
2014-09-06 18:50:35 +0200

    cambio a listings los codigos del capitulo 2

---
+++ Codigos/ccCap2.cpp
@@ -0,0 +1,34 @@
+int foo(int a, int b, ...){
+	...
+}
+
+return y + x;
+
+int w, q, l;
+int j=0;
+
+if(y>0){
+	...
+} else{
+	...
+}
+
+while(w>0){
+	...
+}
+
+for (int i=0; i<x, i=i+1){
+	...
+}
+
+a = b + c;
+
+a *= b;
+
+w = y;
+
+r = 1;
+
+"Hola mundo"
+
+a = foo(b);
